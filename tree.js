be4e054926ea238e7363c643fcb41ba98fc5161b
Grace Bethany Hoober <grace.hoober66@gmail.com>
2023-11-08 11:07:06 -0800

    sumValues debugging

--- tree.js
+++ tree.js
@@ -10,8 +10,25 @@ class TreeNode {
 
   /** sumValues(): add up all values of invoking node and its children.
    * Returns sum as an integer. */
-  sumValues() {
 
+  /** 1 -> 2 -> 3
+   * sum = 6
+   * []
+   * current = 1
+   */
+  sumValues() {
+    let sum = 0;
+    let stack = [this];
+
+    while(stack.length > 0){
+      let current = stack.pop();
+      sum += current.val;
+
+      for(let child of current.children){
+        stack.push(child)
+      }
+    }
+    return sum;
   }
 
   /** countEvens(): starting from the invoking node and moving through its
